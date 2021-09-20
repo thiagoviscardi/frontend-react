6a8f7ffdd8fa5b4c02cd4fd62ba0661a60efe550
thiagoviscardi <thiago.aviscardi@gmail.com>
2021-09-20 13:26:19 -0300

    postcontainer de volta

--- src/components/PostsList/style.js
+++ src/components/PostsList/style.js
@@ -1,6 +1,17 @@
 import styled from 'styled-components';
 
 
+export const PostContainer = styled.div`
+  width: 100%;
+  position: relative;
+  padding: 0 0px;
+  padding-top: 11px;
+  color:#000;
+  text-align:left;
+  background-color:#FFF;
+  border: solid 10px #fff;
+`;
+
 export const HeadTitle = styled.div`
   width: 100%;
   padding: 10px 0px;
